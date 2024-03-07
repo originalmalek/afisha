ea83b31629a858f5886ae82a9a153eaa9ad8869a
originalmalek <originalmalek@gmail.com>
2024-03-07 15:52:14 +0100

    Fill db from json

---
+++ places/json_to_db.py
@@ -0,0 +1,28 @@
+import json
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'wheretogo.settings'
+import django
+django.setup()
+
+from places.models import Place
+
+def store_json_to_db(json_data):
+    new_place = Place(title=json_data['title'],
+                      description_short=json_data['description_short'],
+                      description_long=json_data['description_long'],
+                      lng=json_data['coordinates']['lng'],
+                      lat=json_data['coordinates']['lat'])
+
+
+    new_place.save()
+
+def save_json_files_to_db(directory_path):
+    for filename in os.listdir(directory_path):
+        if filename.endswith('.json'):
+            with open(os.path.join(directory_path, filename), 'r') as f:
+                json_data = json.load(f)
+                print(json_data)
+        store_json_to_db(json_data)
+
+
+save_json_files_to_db('places')
