fe1ed9ae2a1288da748353f9d1480da7f1a49bfe
originalmalek <originalmalek@gmail.com>
2024-03-08 12:58:49 +0100

    Add model for images

--- places/models.py
+++ places/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 
+
 class Place(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, verbose_name='Название экскурсии')
     description_short = models.TextField()
     description_long = models.TextField()
     lng = models.CharField(max_length=20)
@@ -10,3 +11,22 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+
+class Image(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    img = models.ImageField(upload_to='places')
+    order = models.PositiveIntegerField(default=0)
+
+
+    class Meta:
+        unique_together = ('place', 'order')
+    def __str__(self):
+        return f'{self.order} {self.place.title}'
+
+    def save(self, *args, **kwargs):
+        if not self.order:  # if order is not set
+            max_order = Image.objects.filter(place=self.place).aggregate(models.Max('order'))['order__max'] or 0
+            self.order = max_order + 1
+        super().save(*args, **kwargs)
