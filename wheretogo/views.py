06f10f2cda8afffc3a436f0319c35b716c5762eb
originalmalek <originalmalek@gmail.com>
2024-03-08 16:44:42 +0100

    Move data from template to view

--- wheretogo/views.py
+++ wheretogo/views.py
@@ -1,7 +1,27 @@
+import json
+
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 
+from places.models import Place
+
 
 def first_page(request):
-    return render(request, 'where/index.html')
+    places = Place.objects.all()
+    places_list = []
+    for place in places:
+        places_list.append({
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [place.lng, place.lat]
+          },
+          "properties": {
+            "title": place.title,
+            "placeId": place.id,
+            "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/moscow_legends.json"
+          }
+        })
+
+    return render(request, 'where/index.html', {'places': json.dumps(places_list)})
