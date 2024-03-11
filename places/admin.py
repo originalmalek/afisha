bd2db0e9d5e95a430a40201b23f73bc0db85c813
originalmalek <originalmalek@gmail.com>
2024-03-11 10:24:04 +0100

    Upload pics from tour card

--- places/admin.py
+++ places/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Place, Image
 
+class ImageInline(admin.TabularInline):
+    model = Image
 
-admin.site.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [
+        ImageInline,
+    ]
+
+admin.site.register(Place, PlaceAdmin)
 admin.site.register(Image)
-
