5ddd77f0be875e86b063213d64e305828edb7939
originalmalek <originalmalek@gmail.com>
2024-03-11 10:41:56 +0100

    Show pics in tour card

--- places/admin.py
+++ places/admin.py
@@ -1,8 +1,17 @@
+from django.utils.html import format_html
 from django.contrib import admin
 from .models import Place, Image
 
 class ImageInline(admin.TabularInline):
     model = Image
+    readonly_fields = ('image_preview',)
+
+    def image_preview(self, obj):
+        if obj.img:  # Обновляем здесь на `img`
+            return format_html('<img src="{}" height="200" />', obj.img.url)  # И здесь на `img`
+        else:
+            return "No Image Found"
+    image_preview.short_description = 'Image Preview'
 
 class PlaceAdmin(admin.ModelAdmin):
     inlines = [
@@ -10,4 +19,3 @@ class PlaceAdmin(admin.ModelAdmin):
     ]
 
 admin.site.register(Place, PlaceAdmin)
-admin.site.register(Image)
