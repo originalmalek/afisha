1a601accb7cfa87ea66afaf28a627fe43a197c6d
originalmalek <originalmalek@gmail.com>
2024-03-12 10:55:13 +0100

    Order pics in card

--- places/admin.py
+++ places/admin.py
@@ -1,21 +1,27 @@
-from django.utils.html import format_html
 from django.contrib import admin
-from .models import Place, Image
+from .models import Image, Place
+from django.utils.safestring import mark_safe
+from adminsortable2.admin import SortableInlineAdminMixin, SortableAdminMixin
 
-class ImageInline(admin.TabularInline):
+IMAGE_WIDTH = 200
+
+@admin.register(Image)
+class AdminImage(admin.ModelAdmin):
+    readonly_fields = ['get_preview']
+    def get_preview(self, obj):
+        return mark_safe('<img src="{url}" height=200 />'.format(
+            url=obj.img.url, ))
+
+class AdminImageInline(SortableInlineAdminMixin, admin.TabularInline):
     model = Image
-    readonly_fields = ('image_preview',)
+    list_display = ('image', 'get_preview',)
+    readonly_fields = ['get_preview']
+    extra = 0
+    def get_preview(self, obj):
+        return mark_safe('<img src="{url}" width={width} />'.format(
+            url=obj.img.url,
+            width=IMAGE_WIDTH, ))
 
-    def image_preview(self, obj):
-        if obj.img:  # Обновляем здесь на `img`
-            return format_html('<img src="{}" height="200" />', obj.img.url)  # И здесь на `img`
-        else:
-            return "No Image Found"
-    image_preview.short_description = 'Image Preview'
-
-class PlaceAdmin(admin.ModelAdmin):
-    inlines = [
-        ImageInline,
-    ]
-
-admin.site.register(Place, PlaceAdmin)
+@admin.register(Place)
+class AdminPlace(SortableAdminMixin, admin.ModelAdmin):
+    inlines = [AdminImageInline]
--- places/models.py
+++ places/models.py
@@ -4,10 +4,14 @@ from django.db import models
 
 class Place(models.Model):
     title = models.CharField(max_length=200, verbose_name='Название экскурсии')
-    description_short = models.TextField()
-    description_long = models.TextField()
-    lng = models.CharField(max_length=20)
-    lat = models.CharField(max_length=20)
+    description_short = models.TextField(verbose_name='Краткое описание')
+    description_long = models.TextField(verbose_name='Полное описание')
+    lng = models.CharField(max_length=20, verbose_name='Долгота')
+    lat = models.CharField(max_length=20, verbose_name='Широта')
+
+
+    class Meta:
+        ordering = ['title']
 
     def __str__(self):
         return self.title
@@ -21,12 +25,13 @@ class Image(models.Model):
 
 
     class Meta:
-        unique_together = ('place', 'order')
+        ordering = ['order']
+
     def __str__(self):
         return f'{self.order} {self.place.title}'
 
     def save(self, *args, **kwargs):
-        if not self.order:  # if order is not set
+        if self.order is None or Image.objects.filter(place=self.place, order=self.order).exists():
             max_order = Image.objects.filter(place=self.place).aggregate(models.Max('order'))['order__max'] or 0
             self.order = max_order + 1
         super().save(*args, **kwargs)
