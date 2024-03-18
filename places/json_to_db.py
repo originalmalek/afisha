2e62c955c7eaf2f2ffc2be69c9d903c87935901b
originalmalek <originalmalek@gmail.com>
2024-03-18 09:41:49 +0100

    upload pics

--- places/json_to_db.py
+++ places/json_to_db.py
@@ -1,20 +1,34 @@
 import json
 import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'wheretogo.settings'
+
 import django
+import requests
+from django.core.files.base import ContentFile
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'wheretogo.settings'
+
 django.setup()
 
-from places.models import Place
+from places.models import Place, Image
+
 
 def store_json_to_db(json_data):
-    new_place = Place(title=json_data['title'],
-                      description_short=json_data['description_short'],
-                      description_long=json_data['description_long'],
-                      lng=json_data['coordinates']['lng'],
+    new_place = Place(title=json_data['title'], description_short=json_data['description_short'],
+                      description_long=json_data['description_long'], lng=json_data['coordinates']['lng'],
                       lat=json_data['coordinates']['lat'])
 
-
     new_place.save()
+
+    for img_url in json_data['imgs']:
+        response = requests.get(img_url)
+
+        if response.status_code == 200:
+            img_data = response.content
+            img_name = img_url.split("/")[-1]  # Get the image name from the url
+
+            new_image = Image(place=new_place)
+            new_image.img.save(img_name, ContentFile(img_data), save=True)
+
 
 def save_json_files_to_db(directory_path):
     for filename in os.listdir(directory_path):
@@ -25,4 +39,4 @@ def save_json_files_to_db(directory_path):
         store_json_to_db(json_data)
 
 
-save_json_files_to_db('places')
+save_json_files_to_db('content')
