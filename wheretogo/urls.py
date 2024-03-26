120fbe761b1a2394c09458c66a0349c6d98c8a4a
originalmalek <originalmalek@gmail.com>
2024-03-26 16:04:18 +0100

    optimize imports

--- wheretogo/urls.py
+++ wheretogo/urls.py
@@ -14,17 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 from places.views import index_page, generate_place_json
 
-from django.conf import settings
-from django.conf.urls.static import static
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index_page),
-    path('place/<int:place_id>/', generate_place_json),
-    path('tinymce/', include('tinymce.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('', index_page),
+                  path('place/<int:place_id>/', generate_place_json),
+                  path('tinymce/', include('tinymce.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
