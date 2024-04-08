fe264046b04b42e0446a2c6d30fe17d4ebb32b44
originalmalek <originalmalek@gmail.com>
2024-04-08 15:46:28 +0200

    Set Static Root

--- wheretogo/urls.py
+++ wheretogo/urls.py
@@ -18,6 +18,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from places.views import index_page, generate_place_json
 
@@ -27,3 +28,5 @@ urlpatterns = [
                   path('place/<int:place_id>/', generate_place_json),
                   path('tinymce/', include('tinymce.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
