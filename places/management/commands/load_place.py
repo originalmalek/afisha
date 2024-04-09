b8c60177df9fbd71b7db8a3e15e43a3b3b1f7695
originalmalek <originalmalek@gmail.com>
2024-04-09 17:20:32 +0200

    Check if place exists

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -9,26 +9,31 @@ class Command(BaseCommand):
     help = 'Upload place from json file'
 
     def add_arguments(self, parser):
-        parser.add_argument('-u', '--url', default=False, help='Url to Json file', )
+        parser.add_argument('-u', '--url', default=False, help='Url to Json file')
 
     def handle(self, *args, **options):
         place_json_url = options['url']
         response = requests.get(place_json_url)
         response.raise_for_status()
         place = response.json()
+        new_place, created = Place.objects.get_or_create(
+            title=place['title'],
+            defaults={
+                'short_description': place['description_short'],
+                'long_description': place['description_long'],
+                'lng': place['coordinates']['lng'],
+                'lat': place['coordinates']['lat']
+            }
+        )
 
-        new_place = Place(title=place['title'], short_description=place['description_short'],
-                          long_description=place['description_long'], lng=place['coordinates']['lng'],
-                          lat=place['coordinates']['lat'])
+        if created:
+            for img_url in place['imgs']:
+                response = requests.get(img_url)
+                if response.ok:
+                    img_data = response.content
+                    img_name = img_url.split("/")[-1]
+                    new_image = Image(place=new_place)
+                    new_image.img.save(img_name, ContentFile(img_data), save=True)
 
-        new_place.save()
-
-        for img_url in place['imgs']:
-            response = requests.get(img_url)
-
-            if response.ok:
-                img_data = response.content
-                img_name = img_url.split("/")[-1]
-
-                new_image = Image(place=new_place)
-                new_image.img.save(img_name, ContentFile(img_data), save=True)
+        else:
+            print("Place wasn't added because it already exists")
