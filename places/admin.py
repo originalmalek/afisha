72feb406fe1db32ff90b1861fcdbc28c7870a02a
originalmalek <originalmalek@gmail.com>
2024-04-29 17:00:25 +0200

    Fix code

--- places/admin.py
+++ places/admin.py
@@ -1,16 +1,18 @@
 from django.contrib import admin
 from .models import Image, Place
-from django.utils.safestring import mark_safe
 from adminsortable2.admin import SortableInlineAdminMixin, SortableAdminMixin
+from django.utils.html import format_html
+
 
 IMAGE_WIDTH = 200
 
 @admin.register(Image)
 class AdminImage(admin.ModelAdmin, SortableAdminMixin):
     readonly_fields = ['get_preview']
+    autocomplete_fields = ['place']
     def get_preview(self, obj):
-        return mark_safe('<img src="{url}" height=200 />'.format(
-            url=obj.img.url, ))
+        def get_preview(self, obj):
+            return format_html('<img src="{}" style="max-height: 200px; max-width: 200px"/>', obj.img.url)
 
 
 class AdminImageInline(SortableInlineAdminMixin, admin.TabularInline):
@@ -18,11 +20,11 @@ class AdminImageInline(SortableInlineAdminMixin, admin.TabularInline):
     list_display = ('image', 'get_preview',)
     readonly_fields = ['get_preview']
     extra = 0
+
     def get_preview(self, obj):
-        return mark_safe('<img src="{url}" width={width} />'.format(
-            url=obj.img.url,
-            width=IMAGE_WIDTH, ))
+        return format_html('<img src="{}" style="max-height: 200px; max-width: 200px"/>', obj.img.url)
 
 @admin.register(Place)
 class AdminPlace(SortableAdminMixin, admin.ModelAdmin):
     inlines = [AdminImageInline]
+    search_fields = ['title', ]
