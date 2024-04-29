e1ab71aca9863444cf2512cbc0196efff5b351c3
originalmalek <originalmalek@gmail.com>
2024-04-29 16:48:16 +0200

    add default hosts

--- wheretogo/settings.py
+++ wheretogo/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 
 DEBUG = env.bool('DEBUG', default=True)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
 
 SESSION_COOKIE_SECURE = env.bool('SESSION_COOKIE_SECURE', default=True)
 CSRF_COOKIE_SECURE = env.bool('CSRF_COOKIE_SECURE', default=True)
